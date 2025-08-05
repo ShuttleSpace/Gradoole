e56d91ec9fcb88c82046baec7e9d2bfe4846af65
GitHub Action <action@github.com>
2025-08-05 03:25:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/shuttlespace/gradoole/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/shuttlespace/gradoole/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.shuttlespace.gradoole.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
